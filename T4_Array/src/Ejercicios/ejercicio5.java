2d92767faa3c4868d2e8292f2deee90428bcd80e
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-15 10:52:18 +0100

    ej

---
+++ T4_Array/src/Ejercicios/ejercicio5.java
@@ -0,0 +1,5 @@
+package Ejercicios;
+
+import
+public class ejercicio5 {
+}
---
+++ T4_Array/src/Ejercicios/ejercicio6.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+
+        //Variables
+
+        //Programa
+
+    }
+}
