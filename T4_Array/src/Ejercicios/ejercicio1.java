599020ca1db9a4014780309a14eb8b51f2d7049d
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-15 09:21:31 +0100

    ejercicio

---
+++ T4_Array/src/Ejercicios/ejercicio1.java
@@ -0,0 +1,20 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+        //Variables
+        int[] numeros =new int[10];
+        Scanner lecturaTeclado =new Scanner(System.in);
+
+        //Programa
+        for(int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce número a guardar");
+            int numero = lecturaTeclado.nextInt();
+            numeros[i]=numero;
+
+        }
+    }
+}
