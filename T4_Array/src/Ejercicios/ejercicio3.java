872aebf31c4f8b262cb62d2579f3432f61180bb2
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-15 10:12:25 +0100

    e

--- T4_Array/src/Ejercicios/ejercicio3.java
+++ T4_Array/src/Ejercicios/ejercicio3.java
@@ -1,7 +1,29 @@
 package Ejercicios;
 
-public class ejercicio3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class ejercicio3 {
+
+    // Escribe un programa que lea 10 números por teclado, los guarde en un array y que luego los muestre:
+    //
+    //en orden inverso, es decir, el primero que se introduce es el último en mostrarse
+    //en orden normal, es decir, el primero que se muestra es el primero que se ha introducido
+
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        Scanner lecturaTeclado = new Scanner(System.in);
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce numero a guardar");
+            int numero = lecturaTeclado.nextInt();
+            numeros[i] = numero;
+        }
+
+        System.out.println("Orden inverso");
+        for(int i = 9;i >-1; i--){
+            System.out.println(numeros[i]+" \t ");
+        }
+
+        System.out.println("Orden normal");
     }
 }
