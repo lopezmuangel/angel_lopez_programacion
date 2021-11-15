d272c6d9a89ec7944dacb4571552109bd1b79e7a
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-15 11:12:22 +0100

    ej

--- T4_Array/src/Ejercicios/ejercicio5.java
+++ T4_Array/src/Ejercicios/ejercicio5.java
@@ -1,5 +1,9 @@
 package Ejercicios;
 
-import
+import java.util.Scanner;
+
 public class ejercicio5 {
+    public static void main(String[] args) {
+
+    }
 }
--- T4_Array/src/Ejercicios/ejercicio6.java
+++ T4_Array/src/Ejercicios/ejercicio6.java
@@ -6,8 +6,27 @@ public class ejercicio6 {
     public static void main(String[] args) {
 
         //Variables
-
+        int [] aleatorios = new int[20];
+        int sumaModificaciones = 0;
         //Programa
-
+        //rellenar
+        for (int i = 0; i < aleatorios.length; i++){
+            //int aleatorio = (int) Math.random()*11;
+            aleatorios[i] = (int) (Math.random()*31);
+        }
+        //evaluar
+        for (int i = 0; i < aleatorios.length; i++){
+            if (aleatorios[i] ==6){
+                aleatorios[i]=8;
+                sumaModificaciones++;
+            }else if (aleatorios[i]==7){
+                aleatorios[i]=15;
+                sumaModificaciones++;
+            }else if (aleatorios[i]==28){
+                aleatorios[i]=10;
+                sumaModificaciones++;
+            }
+        }
+        System.out.printf("El numero de modificaciones es: %d%n",sumaModificaciones);
     }
 }
