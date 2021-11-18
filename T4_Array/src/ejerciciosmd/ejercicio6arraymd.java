8159f61b5c710c320f7ffea6f431e36ac48eb1fe
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-18 11:21:53 +0100

    6

--- T4_Array/src/ejerciciosmd/ejercicio6arraymd.java
+++ T4_Array/src/ejerciciosmd/ejercicio6arraymd.java
@@ -1,15 +1,25 @@
 package ejerciciosmd;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.Scanner;
 
 public class ejercicio6arraymd {
     public static void main(String[] args) {
-        int[]arrayUno , arrayDos , arrayTres;
-        int longitud;
-        Scanner lecturaTeclado =new Scanner(System.in);
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        System.out.println("De que longitud quieres los arrays");
-        longitud = lecturaTeclado.nextInt();
-        
+        int ultimo = numeros[numeros.length - 1];
+        int primero = numeros[0];
+        for (int i = 0; i < numeros.length - 1; i++) {
+            numeros[i] = numeros[i + 1];
+        }
+        numeros[numeros.length - 1] = primero;
+        for (int i = numeros.length - 1; i > 0; i--) {
+            numeros[i] = numeros[i - 1];
+        }
+        numeros[0] = ultimo;
+        for (int item : numeros) {
+            System.out.print(item + " ");
+        }
+
     }
 }
