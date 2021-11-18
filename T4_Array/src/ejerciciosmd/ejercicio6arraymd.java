18aafc4f26b440fbad04d3d13db01cfce7804e0a
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-18 10:57:31 +0100

    6

--- T4_Array/src/ejerciciosmd/ejercicio6arraymd.java
+++ T4_Array/src/ejerciciosmd/ejercicio6arraymd.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class ejercicio6arraymd {
     public static void main(String[] args) {
+        int[]arrayUno , arrayDos , arrayTres;
+        int longitud;
+        Scanner lecturaTeclado =new Scanner(System.in);
 
-
+        System.out.println("De que longitud quieres los arrays");
+        longitud = lecturaTeclado.nextInt();
+        
     }
 }
