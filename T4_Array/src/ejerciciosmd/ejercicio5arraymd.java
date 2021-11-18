8643b731e568ee91bf9b93f6e670ef06e935ec46
lopezmuangel <92918275+lopezmuangel@users.noreply.github.com>
2021-11-18 10:46:18 +0100

    5

---
+++ T4_Array/src/ejerciciosmd/ejercicio5arraymd.java
@@ -0,0 +1,29 @@
+package ejerciciosmd;
+
+import java.util.Scanner;
+
+public class ejercicio5arraymd {
+    public static void main(String[] args) {
+        int[]arrayUno , arrayDos , arrayTres;
+        int longitud;
+        Scanner lecturaTeclado =new Scanner(System.in);
+
+        System.out.println("De que longitud quieres los arrays");
+        longitud = lecturaTeclado.nextInt();
+
+        arrayUno = new int [longitud];
+        arrayDos = new int [longitud];
+        arrayTres = new int [longitud];
+
+        for (int i = 0; i< longitud; i++){
+            arrayUno[i] = (int)(Math.random()*21);
+            arrayDos[i] = (int)(Math.random()*21);
+        }
+
+        for (int i = 0; i < longitud; i++){
+            arrayTres[i] =arrayUno[i] + arrayDos[i];
+        }
+
+
+    }
+}
